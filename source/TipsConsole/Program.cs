2d1aae25e3dd5c865204d92190a3ca481fff2d15
Xiaodi Yan <yan_xiaodi@hotmail.com>
2023-04-26 23:04:49 +1200

    Initial commit

---
+++ source/TipsConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TipsConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			// a comment that explains something.
+		}
+		static void AddKeyboardShortcut()
+		{
+
+			Console.WriteLine("Add a keyboard shortcut");
+			Console.WriteLine("to do a Git pull operation");
+
+
+		}
+		public void ShowDuplicateTool()
+		{
+			Console.WriteLine("One");
+			Console.WriteLine("Two");
+
+			Console.WriteLine("Three");
+			Console.WriteLine("Four");
+		}
+	}
+}
