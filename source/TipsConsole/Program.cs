2636fa590078d83cb02ae9beda08edea26910f06
Xiaodi Yan <yan_xiaodi@hotmail.com>
2023-05-28 15:25:51 +1200

    Add debug string viewer

--- source/TipsConsole/Program.cs
+++ source/TipsConsole/Program.cs
@@ -8,6 +8,30 @@ internal class Program
     {
 
         // a comment that explains something.
+        // Generate a jwt token
+        string token = GenerateJwtToken();
+        Console.WriteLine(token);
+        // Generate a sample for base64 encoding
+        string base64String = GenerateBase64String("Hello World! Welcome to Visual Studio Tips.");
+        Console.WriteLine(base64String);
+        // Generate a sample for url encoding
+        string urlEncodedString = GenerateUrlEncodedString("https://www.bing.com/search?q=dotnet");
+        Console.WriteLine(urlEncodedString);
+    }
+
+    private static string GenerateJwtToken()
+    {
+        return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+    }
+
+    private static string GenerateBase64String(string content)
+    {
+        return Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(content));
+    }
+
+    private static string GenerateUrlEncodedString(string content)
+    {
+        return System.Net.WebUtility.UrlEncode(content);
     }
 
     private static void AddKeyboardShortcut()
@@ -26,11 +50,4 @@ internal class Program
         Console.WriteLine("Three");
         Console.WriteLine("Four");
     }
-
-
-
-
-
-
-
 }
